fa9bae80891572491e85862b0921396f42e2e5cf
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-09-10 11:35:48 +0200

    refix code, toast added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import './question.dart';
 import './answer.dart';
@@ -15,33 +16,45 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favourite book?',
+      'answers': [
+        'Harry Potter',
+        'Everything about flutter',
+        'Better than yesterday',
+        'Cool and the gang'
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Rabbit', 'Snake', 'Dog', 'Cat'],
+    },
+  ];
+
   void _answerQuestion() {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
+    if (_questionIndex < questions.length - 1) {
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+    } else {
+      Fluttertoast.showToast(
+          msg: "We are out of questions, thank you for Quizz",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 2,
+          backgroundColor: Colors.white60,
+          textColor: Colors.blueGrey,
+          fontSize: 16.0);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favourite book?',
-        'answers': [
-          'Harry Potter',
-          'Everything about flutter',
-          'Better than yesterday',
-          'Cool and the gang'
-        ],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Rabbit', 'Snake', 'Dog', 'Cat'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Quizzzz')),
