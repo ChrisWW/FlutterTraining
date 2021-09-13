52a4d8ea847d9efc7d45cd32f68cdcedc8a6dfa9
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-09-13 17:06:41 +0200

    clean code, adding score

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,39 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
   final _questions = const [
     {
       'questionText': 'What\'s your favourite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 8},
+        {'text': 'Green', 'score': 5},
+        {'text': 'White', 'score': 3}
+      ],
     },
     {
       'questionText': 'What\'s your favourite book?',
       'answers': [
-        'Harry Potter',
-        'Everything about flutter',
-        'Better than yesterday',
-        'Cool and the gang'
+        {'text': 'Harry Potter', 'score': 1},
+        {'text': 'Everything about flutter', 'score': 1},
+        {'text': 'Better than yesterday', 'score': 1},
+        {'text': 'Cool and the gang', 'score': 1}
       ],
     },
     {
       'questionText': 'What\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Dog', 'Cat'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 1}, {'text': 'Snake', 'score': 1},
+        {'text': 'Dog', 'score': 2}, {'text': 'Cat', 'score': 3}
+      ],
     },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     if (_questionIndex < _questions.length) {
       setState(() {
         _questionIndex = _questionIndex + 1;
